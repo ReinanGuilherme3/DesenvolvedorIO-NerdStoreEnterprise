5e5635249d1dcd950f9affb49ff0322fa90c499e
reinan.neto <reinan.neto@grupofasitec.com.br>
2025-12-22 11:36:37 -0300

    Emitindo JWT pela controller

--- src/Services/Identidade/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/Services/Identidade/NSE.Identidade.API/Controllers/AuthController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NSE.Identidade.API.Extensions;
 using NSE.Identidade.API.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace NSE.Identidade.API.Controllers;
 
@@ -10,11 +15,16 @@ public class AuthController : ControllerBase
 {
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly AppSettings _appSettings;
 
-    public AuthController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    public AuthController(
+        UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager,
+        IOptions<AppSettings> appSettings)
     {
         _userManager = userManager;
         _signInManager = signInManager;
+        _appSettings = appSettings.Value;
     }
 
     [HttpPost("nova-conta")]
@@ -34,7 +44,7 @@ public class AuthController : ControllerBase
         if (result.Succeeded)
         {
             await _signInManager.SignInAsync(user, false);
-            return Ok();
+            return Ok(await GerarJwt(request.Email));
         }
 
         return BadRequest(result.Errors);
@@ -49,9 +59,63 @@ public class AuthController : ControllerBase
 
         if (result.Succeeded)
         {
-            return Ok();
+            return Ok(await GerarJwt(request.Email));
         }
 
         return BadRequest("Usuário ou senha inválidos");
+    }
+
+    private async Task<UsuarioRespostaLogin> GerarJwt(string email)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+        var claims = await _userManager.GetClaimsAsync(user);
+        var userRoles = await _userManager.GetRolesAsync(user);
+
+        claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString())); // quando vai expirar
+        claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64)); // quando foi emitido
+
+        foreach (var userRole in userRoles)
+        {
+            claims.Add(new Claim("role", userRole));
+        }
+
+        var identityClaims = new ClaimsIdentity();
+        identityClaims.AddClaims(claims);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = System.Text.Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+        var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+        {
+            Issuer = _appSettings.Emissor,
+            Audience = _appSettings.ValidoEm,
+            Subject = identityClaims,
+            Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        });
+
+        var encodedToken = tokenHandler.WriteToken(token);
+
+        return new UsuarioRespostaLogin
+        {
+            AccessToken = encodedToken,
+            ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+            UsuarioToken = new UsuarioToken
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Claims = claims.Select(c => new UsuarioClaim { Type = c.Type, Value = c.Value })
+            }
+        };
+
+    }
+
+    private static long ToUnixEpochDate(DateTime date)
+    {
+        return (long)Math.Round((date.ToUniversalTime() -
+            new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
     }
 }
