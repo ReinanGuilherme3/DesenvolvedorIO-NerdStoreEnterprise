d71e665baaaeb88d95be233564c78abbe5087e2d
reinan.neto <reinan.neto@grupofasitec.com.br>
2025-12-23 14:13:34 -0300

    Command e Command Handler

---
+++ src/Services/Cliente/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace NSE.Cliente.API.Application.Commands;
+
+public class ClienteCommandHandler
+{
+    public void Manipular(RegistrarClienteCommand command)
+    {
+        // Lógica para manipular o comando de registrar cliente
+    }
+}
