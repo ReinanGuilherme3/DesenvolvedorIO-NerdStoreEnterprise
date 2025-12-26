5a39d85ff6223c1aac2f13c9090ff3baed4405c6
ReinanGuilherme3 <r.guilhermedeoliveira3@gmail.com>
2025-12-26 11:09:37 -0300

    Execução do processo de cadastro

--- src/Building Blocks/Services/NSE.WebApi.Core/Controllers/MainController.cs
+++ src/Building Blocks/Services/NSE.WebApi.Core/Controllers/MainController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace NSE.WebApi.Core.Controllers;
@@ -25,6 +26,15 @@ public abstract class MainController : ControllerBase
     {
         var erros = modelState.Values.SelectMany(e => e.Errors);
         foreach (var erro in erros)
+        {
+            AdicionarErroProcessamento(erro.ErrorMessage);
+        }
+        return CustomResponse();
+    }
+
+    protected IActionResult CustomResponse(ValidationResult validationResult)
+    {
+        foreach (var erro in validationResult.Errors)
         {
             AdicionarErroProcessamento(erro.ErrorMessage);
         }
